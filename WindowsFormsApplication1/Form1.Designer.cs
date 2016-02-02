afbeda0f41ef42c5225238432f4b3b92b2d6eea7
forthxu <forthxu@gmail.com>
2016-02-02 15:41:31 +0800

    update 初始化

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿using System;
+namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(510, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(181, 7);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(323, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "http://4399.com";
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_KeyDown);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(2, 41);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1262, 666);
+            this.webBrowser1.TabIndex = 2;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            this.webBrowser1.DocumentTitleChanged += new System.EventHandler(this.webBrowser1_DocumentTitleChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(2, 8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(60, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "HOME";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(68, 8);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(47, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "<<";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(121, 8);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(42, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = ">>";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(678, 8);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(98, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "skynetdoc.com";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(788, 8);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(85, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "forthxu.com";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(887, 8);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "4399.com";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(972, 8);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "行动派";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(594, 8);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "refresh";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 707);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "forthxu\'s browser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Text = "Forthxu\'s Browser";
+
+            webBrowser1.ScriptErrorsSuppressed = true; //禁用错误脚本提示
+            webBrowser1.IsWebBrowserContextMenuEnabled = false; //禁用右键菜单
+            webBrowser1.WebBrowserShortcutsEnabled = false; //禁用快捷键
+            //WindowState = FormWindowState.Maximized;//最大化
+            // MessageBox.Show("sad");
+
+            this.gotourl();
+        }
+
+        private int gotourl(string url="")
+        {
+            if(url=="")
+            {
+                url = textBox1.Text.Trim();
+            }
+            
+            //正则判断
+            string reg = @"^((http|ftp|https)://)?(([a-zA-Z0-9\._-]+\.[a-zA-Z]{2,6})|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,4})*(/[a-zA-Z0-9\&%_\./-~-]*)?";
+            Regex r = new Regex(reg);
+            //给网址去所有空格
+            Match m = r.Match(url);
+
+            //判断是否带http://
+            if (!m.Success)
+                url="http://baidu.com";
+            //给不带http://开头的加上 
+            url = url.Replace("http://", "");
+            url = url.Insert(0, "http://");
+
+            webBrowser1.Url = new Uri(url);
+            WaitWebPageLoad();
+            this.textBox1.Text = this.webBrowser1.Url.ToString();
+            this.Text = this.webBrowser1.Document.Title + "-Forthxu\'s Browser";
+
+            return 1;
+        }
+
+        //窗口resize之前的大小
+        private Size beforeResizeSize = Size.Empty;
+        //改变窗口大小之前自发的事件
+        protected override void OnResizeBegin(EventArgs e)
+        {
+            base.OnResizeBegin(e);
+            beforeResizeSize = this.Size;
+        }
+        //改变窗口大小之后自发的事件
+        protected override void OnResizeEnd(EventArgs e)
+        {
+            base.OnResizeEnd(e);
+            //窗口resize之后的大小
+            Size endResizeSize = this.Size;
+            //获得变化比例
+            float percentWidth = (float)endResizeSize.Width / beforeResizeSize.Width;
+            float percentHeight = (float)endResizeSize.Height / beforeResizeSize.Height;
+            //按比例改变控件大小
+            this.webBrowser1.Width = (int)(webBrowser1.Width * percentWidth);
+            this.webBrowser1.Height = (int)(webBrowser1.Height * percentHeight);
+            //为了不使控件之间覆盖 位置也要按比例变化
+            this.webBrowser1.Left = (int)(webBrowser1.Left * percentWidth);
+            this.webBrowser1.Top = (int)(webBrowser1.Top * percentHeight);
+        }
+
+        //回车
+        private void textBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                this.gotourl();
+            }
+        }
+
+        //go
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.gotourl();
+        }
+
+        //home
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.gotourl("http://baidu.com");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.gotourl("http://skynetdoc.com");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.gotourl("http://forthxu.com");
+        }
+
+        //back
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.webBrowser1.GoBack();
+        }
+        //prev
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.webBrowser1.GoForward();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            this.gotourl("http://4399.com");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            this.gotourl("http://dreamlist.cc");
+        }
+
+        
+        private void Delay(int Millisecond) //延迟系统时间，但系统又能同时能执行其它任务；
+        {
+            DateTime current = DateTime.Now;
+            while (current.AddMilliseconds(Millisecond) > DateTime.Now)
+            {
+                Application.DoEvents();//转让控制权            
+            }
+            return;
+        }
+        private bool WaitWebPageLoad()
+        {
+            int i = 0;
+            string sUrl;
+            while (true)
+            {
+                Delay(50);  //系统延迟50毫秒，够少了吧！             
+                if (webBrowser1.ReadyState == WebBrowserReadyState.Complete) //先判断是否发生完成事件。
+                {
+                    if (!webBrowser1.IsBusy) //再判断是浏览器是否繁忙                  
+                    {
+                        i = i + 1;
+                        if (i == 2)
+                        {
+                            sUrl = webBrowser1.Url.ToString();
+                            if (sUrl.Contains("res")) //这是判断没有网络的情况下                           
+                            {
+                                return false;
+                            }
+                            else
+                            {
+                                return true;
+                            }
+                        }
+                        continue;
+                    }
+                    i = 0;
+                }
+            }
+        }
+
+        //title change
+        private void webBrowser1_DocumentTitleChanged(object sender, EventArgs e)
+        {
+            this.Text = webBrowser1.DocumentTitle + "-Forthxu\'s Browser";
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            //将所有的链接的目标，指向本窗体
+            foreach (HtmlElement archor in this.webBrowser1.Document.Links)
+            {
+                archor.SetAttribute("target", "_self");
+            }
+
+            //将所有的FORM的提交目标，指向本窗体
+            foreach (HtmlElement form in this.webBrowser1.Document.Forms)
+            {
+                form.SetAttribute("target", "_self");
+            }
+        }
+
+        private void webBrowser1_NewWindow(object sender, CancelEventArgs e)
+        {
+            e.Cancel = true;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            this.webBrowser1.Refresh();
+        }
+
+
+    }
+}
